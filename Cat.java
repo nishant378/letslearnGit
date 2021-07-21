45af977d25ab75ab6dd3f2800244b16c5a158b54
Nishant Kumar <nk5374890@gmail.com>
2021-07-21 13:45:19 +0530

    intial commit

---
+++ Cat.java
@@ -0,0 +1,22 @@
+package decision_making;
+
+public class Cat {
+public void  speak()
+{
+	System.out.println("Meow!!!!!!");
+}
+	public static void main(String[] args) {
+		boolean isbool=true;
+		Cat c=new Cat();
+         if(isbool)
+         {
+        	 c.speak();
+        	 
+         }
+         else
+         {
+        	 System.out.println("Cann't speak!!!");
+         }
+	}//new change in dev branch
+//in dev branch
+}
